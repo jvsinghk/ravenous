0c723c48c995ed9f2fb57c3dc090af5e77dda6d7
Jasvinder Singh <jasvinder15atech@gmail.com>
2020-09-05 17:10:29 +0530

    added bussiness component

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Business from '../Business/Business'
+
+function App() {
+  return (
+    <div className="App">
+    <Business />
+    </div>
+  );
+}
+
+export default App;
