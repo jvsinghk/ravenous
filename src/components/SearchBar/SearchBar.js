6669879bdd3f0bb28f5d71f4c8f28bfe2116a224
Jasvinder Singh <jasvinder15atech@gmail.com>
2020-09-06 17:35:39 +0530

    get a sort option's class

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,46 +1,52 @@
 import React, { Component } from "react";
 import "./SearchBar.css";
 
-const sortByOptions = {
-  "Best Match": "best_match",
-  "Highest Rated": "rating",
-  "Most Reviewed": "review_count",
-};
-
 class SearchBar extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      term: '',  //will refer to search term
-      location: '', //will refer to location to search near from the location
-      sortBy: 'best_match', //will represent the selected sorting option to use
+      term: "", //will refer to search term
+      location: "", //will refer to location to search near from the location
+      sortBy: "best_match", //will represent the selected sorting option to use
+    };
+    this.sortByOptions = {
+      "Best Match": "best_match",
+      "Highest Rated": "rating",
+      "Most Reviewed": "review_count",
+    };
+  }
+
+  getSortByClass(sortByOption){
+    if(this.state.sortBy === sortByOption){
+      return "active"
+    }
+    else{
+      return ""
     }
   }
 
   renderSortByOptions() {
-    return Object.keys(sortByOptions).map(sortByOption => {
-      let sortByOptionValue = sortByOptions[sortByOption];
+    return Object.keys(this.sortByOptions).map((sortByOption) => {
+      let sortByOptionValue = this.sortByOptions[sortByOption];
       return <li key={sortByOptionValue}>{sortByOption}</li>;
     });
   }
 
   render() {
-      return (
-       <div className="SearchBar">
+    return (
+      <div className="SearchBar">
         <div className="SearchBar-sort-options">
-            <ul>
-                {this.renderSortByOptions()}
-            </ul>
+          <ul>{this.renderSortByOptions()}</ul>
         </div>
         <div className="SearchBar-fields">
-        <input placeholder="Search Businesses" />
-        <input placeholder="Where?" />
+          <input placeholder="Search Businesses" />
+          <input placeholder="Where?" />
         </div>
         <div className="SearchBar-submit">
-        <a>Let's Go</a>
+          <a>Let's Go</a>
         </div>
-       </div>
-      )
+      </div>
+    );
   }
 }
 
