43f6a1464d281f648f7223597fea6b9bf9a1de3f
Jasvinder Singh <jasvinder15atech@gmail.com>
2020-09-06 18:05:19 +0530

    set the className of sort option

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -16,25 +16,31 @@ class SearchBar extends Component {
     };
   }
 
-  getSortByClass(sortByOption){
-    if(this.state.sortBy === sortByOption){
-      return "active"
-    }
-    else{
-      return ""
+  getSortByClass(sortByOption) {
+    if (sortByOption === this.state.sortBy) {
+      return "active";
+    } else {
+      return "";
     }
   }
 
-  handelSortByChange(sortByOption){
+  handelSortByChange(sortByOption) {
     this.setState({
-      sortBy: sortByOption
-    })
+      sortBy: sortByOption,
+    });
   }
 
   renderSortByOptions() {
     return Object.keys(this.sortByOptions).map((sortByOption) => {
       let sortByOptionValue = this.sortByOptions[sortByOption];
-      return <li key={sortByOptionValue}>{sortByOption}</li>;
+      return (
+        <li
+          key={sortByOptionValue}
+          className={this.getSortByClass(sortByOption)}
+        >
+          {sortByOption}
+        </li>
+      );
     });
   }
 
