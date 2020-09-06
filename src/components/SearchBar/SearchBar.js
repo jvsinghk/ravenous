985cc288731319296c07b6c1ec1e834ca6ad26ce
Jasvinder Singh <jasvinder15atech@gmail.com>
2020-09-06 20:43:12 +0530

    fixed achor tag console warning

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -80,7 +80,7 @@ class SearchBar extends React.Component {
           <input placeholder="Where?" onChange={this.handleLocationChange} />
         </div>
         <div className="SearchBar-submit">
-          <a onClick={this.handleSearch}>Let's Go</a>
+          <a href=" " onClick={this.handleSearch}>Let's Go</a>
         </div>
       </div>
     );
