1cb718c172f946ee6012518ff986379171d86208
Jasvinder Singh <jasvinder15atech@gmail.com>
2020-09-24 00:40:06 +0530

    accessed the json responce

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,19 +1,39 @@
 const apiKey = process.env.Yelp_Api_Key;
 
-const yelp {
-  searchYelp (term, location, sortBy) {
-    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
-      headers: {
-        Authorization: `Bearer ${apiKey}`
-      }   
-    }).then(response => {
-      return response.json();
-    }).then(jsonResponse => {
-      if(jsonResponse.businesses){
-        return jsonRespone.businesses.map(business => {
-
-        });
+const yelp = {
+  searchYelp(term, location, sortBy) {
+    return fetch(
+      `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+      {
+        headers: {
+          Authorization: `Bearer ${apiKey}`,
+        },
       }
-    }) ;
-  }
-}
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((jsonResponse) => {
+        if (jsonResponse.businesses) {
+          console.log(jsonResponse.business);
+          return jsonResponse.businesses.map((business) => {
+            console.log(business);
+            return {
+              id: business.id,
+              imageSrc: business.image_url,
+              name: business.name,
+              address: business.location.address1,
+              city: business.location.city,
+              state: business.location.state,
+              zipCode: business.location.zip_code,
+              category: business.category[0].title,
+              rating: business.rating,
+              reviewCount: business.review_count,
+            };
+          });
+        }
+      });
+  },
+};
+
+export default yelp;
